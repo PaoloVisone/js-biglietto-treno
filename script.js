90cec30db4ce3e6e7482299bd3a1a62f3e74f926
PaoloVisone <polvis.pv.22@gmail.com>
2024-12-17 13:56:37 +0100

    Start project

---
+++ script.js
@@ -0,0 +1,19 @@
+// chiedere il numero dei chilometri ;
+// chiedere l'età;
+// calcolare il prezzo totale;
+// calcola lo sconto per minorenni;
+// calcola lo sconto per over 65;
+
+
+// Dati
+let prezzo = 0.21;
+const scontoMin = 20;
+const scontoMax = 40;
+
+// Prompt
+let eta = prompt("Inserisci l'eta");
+let chilometri = prompt("Inserisci i chilometri");
+
+// Calcoli
+
+
