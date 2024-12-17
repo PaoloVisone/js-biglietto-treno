ce4abf512531d8c6fd73d47ef04d90c49425872f
PaoloVisone <polvis.pv.22@gmail.com>
2024-12-17 15:06:37 +0100

    Add logcal structure

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ const scontoMax = 40;
 let eta = prompt("Inserisci l'eta");
 let chilometri = prompt("Inserisci i chilometri");
 
+console.log(eta);
+console.log(chilometri);
+
 // Calcoli
+
+let somma = prezzo * chilometri;
+
+console.log(somma + " " + "€");
+
+let scontOne = somma * scontoMin / 100;
+
+let scontTwo = somma * scontoMax / 100;
+
+console.log(scontOne);
+console.log(scontTwo);
+
+
+if (eta < 18) {
+    console.log(somma - scontOne + " " + "€");
+} else if (eta > 65) {
+    console.log(somma - scontTwo + " " + "€");
+}
+
+
+
 
 
