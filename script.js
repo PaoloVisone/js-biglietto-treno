c8a98d7f83efa3f3bbf7e39e0537edb743a6c0c3
PaoloVisone <polvis.pv.22@gmail.com>
2024-12-17 16:36:44 +0100

    Add human output ${function}

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 // calcola lo sconto per minorenni;
 // calcola lo sconto per over 65;
 
-
 // Dati
 let prezzo = 0.21;
 const scontoMin = 20;
@@ -14,8 +13,8 @@ const scontoMax = 40;
 let eta = prompt("Inserisci l'eta");
 let chilometri = prompt("Inserisci i chilometri");
 
-console.log(eta);
-console.log(chilometri);
+// console.log(eta);
+// console.log(chilometri);
 
 // Calcoli
 
@@ -25,18 +24,27 @@ console.log(somma + " " + "€");
 
 let scontOne = somma * scontoMin / 100;
 
-let scontTwo = somma * scontoMax / 100;
+let sconTwo = somma * scontoMax / 100;
 
 console.log(scontOne);
-console.log(scontTwo);
+console.log(sconTwo);
 
 
-if (eta < 18) {
-    console.log(somma - scontOne + " " + "€");
-} else if (eta > 65) {
-    console.log(somma - scontTwo + " " + "€");
+// if (eta < 18) {
+//     console.log("Il biglietto costa" + somma - scontOne + " " + "€ !");
+// } else if (eta > 65) {
+//     console.log("Il biglietto costa" + somma - sconTwo + " " + "€ !");
+// }
+
+function risultato() {
+    if (eta < 18) {
+        document.getElementById("demo").innerHTML = `Il biglietto costa ${somma - scontOne} € !`;
+    } else if (eta > 65) {
+        document.getElementById("demo").innerHTML = `Il biglietto costa ${somma - scontOne} € !`;
+    } else {
+        document.getElementById("demo").innerHTML = `Il biglietto costa ${somma} € !`;
+    }
 }
-
 
 
 
